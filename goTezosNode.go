2a1299a930709177376c39d233da5874b40561bc
Brian Bland <brian.bland@coinbase.com>
2019-03-14 16:48:05 -0700

    Reuse http client in tezos rpc client

--- goTezosNode.go
+++ goTezosNode.go
@@ -12,24 +12,25 @@ import (
 )
 
 type TezosRPCClient struct {
-	Host          string
-	Port          string
-	logfunction   func(level, msg string)
-	logger        *log.Logger
-	isWebClient   bool
+	Host        string
+	Port        string
+	logfunction func(level, msg string)
+	logger      *log.Logger
+	isWebClient bool
+	httpClient  *http.Client
 }
 
 // Create a new RPC client using the specified hostname and port.
 // Also acceptable is the hostname of a web-endpoint that supports https
 func NewTezosRPCClient(hostname string, port string) *TezosRPCClient {
 	t := TezosRPCClient{}
-	
+
 	// Strip off posible trailing '/'
 	hLen := len(hostname)
-	if hostname[hLen - 1] == '/' {
-		hostname = hostname[:hLen - 1]
+	if hostname[hLen-1] == '/' {
+		hostname = hostname[:hLen-1]
 	}
-	
+
 	// Strip off URI scheme
 	if hostname[:8] == "https://" {
 		hostname = hostname[8:]
@@ -37,13 +38,26 @@ func NewTezosRPCClient(hostname string, port string) *TezosRPCClient {
 	} else if hostname[:7] == "http://" {
 		hostname = hostname[7:]
 	}
-	
+
 	t.Host = hostname
 	t.Port = port
 	t.logfunction = func(level, msg string) {
 		fmt.Println(level + ": " + msg)
 	}
 	t.SetLogger(log.New(os.Stdout, hostname, 0))
+
+	var netTransport = &http.Transport{ // TODO make this as config option, but with defaults like this
+		Dial: (&net.Dialer{
+			Timeout: 3 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 3 * time.Second,
+	}
+
+	t.httpClient = &http.Client{
+		Timeout:   time.Second * 3,
+		Transport: netTransport,
+	}
+
 	return &t
 }
 
@@ -57,15 +71,15 @@ func (this *TezosRPCClient) IsWebClient(b bool) {
 }
 
 func (this *TezosRPCClient) GetResponse(method string, path string, args string) (ResponseRaw, error) {
-	
+
 	var url string
-	
+
 	if this.isWebClient {
 		url = fmt.Sprintf("https://%s:%s%s", this.Host, this.Port, path)
 	} else {
 		url = fmt.Sprintf("http://%s:%s%s", this.Host, this.Port, path)
 	}
-	
+
 	var jsonStr = []byte(args)
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
 	if err != nil {
@@ -73,19 +87,7 @@ func (this *TezosRPCClient) GetResponse(method string, path string, args string)
 		return ResponseRaw{}, err
 	}
 
-	var netTransport = &http.Transport{ // TODO make this as config option, but with defaults like this
-		Dial: (&net.Dialer{
-			Timeout: 3 * time.Second,
-		}).Dial,
-		TLSHandshakeTimeout: 3 * time.Second,
-	}
-
-	var netClient = &http.Client{
-		Timeout:   time.Second * 3,
-		Transport: netTransport,
-	}
-
-	resp, err := netClient.Do(req)
+	resp, err := this.httpClient.Do(req)
 	if err != nil {
 		this.logger.Println("Error in GetResponse: " + err.Error())
 		return ResponseRaw{}, err
@@ -96,7 +98,6 @@ func (this *TezosRPCClient) GetResponse(method string, path string, args string)
 		this.logger.Println("Error in GetResponse - readAll bytes: " + err.Error())
 		return ResponseRaw{}, err
 	}
-	netTransport.CloseIdleConnections()
 	defer resp.Body.Close()
 	return ResponseRaw{b}, nil
 }
